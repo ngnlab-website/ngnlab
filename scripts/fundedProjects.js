5bb869239792655b846d7192eb95452b47d87bf7
Sudarsan Kumar <74944040+sudarsankumar@users.noreply.github.com>
2023-05-17 11:08:10 +0530

    Fixed all bugs
    Co-Authored-By: Krishna2752 <41601961+Krishna2752@users.noreply.github.com>
    Co-Authored-By: Thilaksurya <79409614+Thilaksurya29@users.noreply.github.com>
    Co-Authored-By: Balakumar R <81617577+balakumarr15@users.noreply.github.com>
    Co-Authored-By: Abhishek Manoharan <64410792+darwinabhi963@users.noreply.github.com>

--- scripts/fundedProjects.js
+++ scripts/fundedProjects.js
@@ -1,13 +1,25 @@
 const fundedProjectsSectionContainer = document.querySelector(".funded-projects-section-container")
-import fundedProjects from "../data/fundedProjects.json" assert { type: "json" }
-fundedProjects.forEach(fundedProject => {
-    let proj =  `
-                    <div class="funded-project"> 
-                        <div class="description">${fundedProject.title}</div>
-                        <div class="other-details">
-                            <span class="funding-agency">${fundedProject.agency}</span>
+// import fundedProjects from "../data/fundedProjects.json" assert { type: "json" }
+let fundedProjects = []
+fetch("../data/fundedProjects.json")
+    .then(response => response.json())
+    .then(data => {
+        fundedProjects=data
+        renderProjects()
+    })
+    .catch(error => {
+        console.error("Error fetching JSON:", error);
+    });
+function renderProjects() {
+    fundedProjects.forEach(fundedProject => {
+        let proj =  `
+                        <div class="funded-project"> 
+                            <div class="description">${fundedProject.title}</div>
+                            <div class="other-details">
+                                <span class="funding-agency">${fundedProject.agency}</span>
+                            </div>
                         </div>
-                    </div>
-                `
-    fundedProjectsSectionContainer.innerHTML += proj
-})
+                    `
+        fundedProjectsSectionContainer.innerHTML += proj
+    })
+}
--- scripts/newsRoom.js
+++ scripts/newsRoom.js
@@ -1,36 +1,48 @@
 const peopleSectionContainer = document.querySelector(".people-section-container")
-import newsRoom from "../data/newsRoom.json" assert { type: "json" }
-newsRoom.forEach((news, index) => {
-    if(index%2 !== 0) {
-        let newsContent =   `
-                            <div class="people-section-cont">
-                                <div class="director even">
-                                    <div class="photo">
-                                        <img src="${news.img}" alt="">
-                                    </div>
-                                    <div class="details">
-                                        <div class="name">${news.title}</div>
-                                        <div class="about">${news.about}</div>
-                                    </div>
-                                </div>
-                            </div>
-                        `
-        peopleSectionContainer.innerHTML += newsContent
-    }
-    else {
-        let newsContent =   `
-                            <div class="people-section-cont">
-                                <div class="director">
-                                    <div class="photo">
-                                        <img src="${news.img}" alt="">
-                                    </div>
-                                    <div class="details">
-                                        <div class="name">${news.title}</div>
-                                        <div class="about">${news.about}</div>
+// import newsRoom from "../data/newsRoom.json" assert { type: "json" }
+let newsRoom = []
+fetch("../data/newsRoom.json")
+    .then(response => response.json())
+    .then(data => {
+        newsRoom=data
+        renderNews()
+    })
+    .catch(error => {
+        console.error("Error fetching JSON:", error);
+    });
+function renderNews () {
+    newsRoom.forEach((news, index) => {
+        if(index%2 !== 0) {
+            let newsContent =   `
+                                <div class="people-section-cont">
+                                    <div class="director even">
+                                        <div class="photo">
+                                            <img src="${news.img}" alt="">
+                                        </div>
+                                        <div class="details">
+                                            <div class="name">${news.title}</div>
+                                            <div class="about">${news.about}</div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        `
-        peopleSectionContainer.innerHTML += newsContent
-    }
-})
+                            `
+            peopleSectionContainer.innerHTML += newsContent
+        }
+        else {
+            let newsContent =   `
+                                <div class="people-section-cont">
+                                    <div class="director">
+                                        <div class="photo">
+                                            <img src="${news.img}" alt="">
+                                        </div>
+                                        <div class="details">
+                                            <div class="name">${news.title}</div>
+                                            <div class="about">${news.about}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            `
+            peopleSectionContainer.innerHTML += newsContent
+        }
+    })
+}
