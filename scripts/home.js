fc4951e8677fedd4c6e05a6825ec8023a4567a18
Sudarsan Kumar <74944040+sudarsankumar@users.noreply.github.com>
2023-05-17 11:38:34 +0530

    Added proper slideshow
    Co-Authored-By: Krishna2752 <41601961+Krishna2752@users.noreply.github.com>
    Co-Authored-By: Thilaksurya <79409614+Thilaksurya29@users.noreply.github.com>
    Co-Authored-By: Balakumar R <81617577+balakumarr15@users.noreply.github.com>
    Co-Authored-By: Abhishek Manoharan <64410792+darwinabhi963@users.noreply.github.com>

--- scripts/home.js
+++ scripts/home.js
@@ -14,3 +14,21 @@ readMoreBtn.addEventListener("click", ()=> {
         moreText.style.display = "inline";
     }
 })
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    x[slideIndex-1].style.display = "block";  
+    setTimeout(showDivs(slideIndex), 2000);
+}
