89a4b6c88900e4d612d7d7254c2ad4e3ed30e9b4
Sudarsan Kumar <74944040+sudarsankumar@users.noreply.github.com>
2023-05-18 11:10:31 +0530

    Updated changes
    Co-Authored-By: Krishna2752 <41601961+Krishna2752@users.noreply.github.com>
    Co-Authored-By: Thilaksurya <79409614+Thilaksurya29@users.noreply.github.com>
    Co-Authored-By: Balakumar R <81617577+balakumarr15@users.noreply.github.com>
    Co-Authored-By: Abhishek Manoharan <64410792+darwinabhi963@users.noreply.github.com>

--- scripts/home.js
+++ scripts/home.js
@@ -14,21 +14,53 @@ readMoreBtn.addEventListener("click", ()=> {
         moreText.style.display = "inline";
     }
 })
-var slideIndex = 1;
-showDivs(slideIndex);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }    
+    if (n < 1) {
+        slideIndex = slides.length
     }
-    x[slideIndex-1].style.display = "block";  
-    setTimeout(showDivs(slideIndex), 2000);
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" actives", "");
+    }
+    slides[slideIndex-1].style.display = "flex";  
+    dots[slideIndex-1].className += " actives";
+    // setTimeout(showSlides(slideIndex-1), 2000);
+}
+let autoSlideIndex = 0;
+autoShowSlides();
+
+function autoShowSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" actives", "");
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += " actives";
+  setTimeout(autoShowSlides, 5000); // Change image every 2 seconds
 }
