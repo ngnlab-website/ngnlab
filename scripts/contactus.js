9653f73f77aa814d1482acdec903884422531b3d
ShivaniSuresh1 <98629661+ShivaniSuresh1@users.noreply.github.com>
2024-04-19 22:46:20 +0530

    Update contactus.js

--- scripts/contactus.js
+++ scripts/contactus.js
@@ -4,6 +4,7 @@ const phdContainer = document.querySelector(".phd")
 const alumniContainer = document.querySelector(".alumni")
 const thirdYearContainer = document.querySelector(".third-year")
 const otherContainer = document.querySelector(".others")
+const passoutContainer = document.querySelector(".passout")
 // import students from "../data/students.json" assert { type: "json" }
 let students = []
 fetch("../data/students.json")
@@ -40,6 +41,29 @@ function renderPeople() {
                             </div>
                         </div>`
             otherContainer.innerHTML += stud
+        }
+        if(student.year === "passout") {
+            let stud = `<div class="student-member-card">
+                            <div class="face face1">
+                                <div class="content">
+                                    <div class="content-details">
+                                        <h3>${student.name}</h3>
+                                        <a href="mailto:${student.mail}">${student.mail}</a>
+                                        <a href="${student.linkedin}" class="linkedin-img" target="_blank">Linkedin</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="face face2">
+                                <div class="content">
+                                    <div class="social-media">
+                                        <div class="name">${student.batch}</div>
+                                        <div class="role">${student.currentPosition}</div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`
+            passoutContainer.innerHTML += stud
         }
     })
 }
