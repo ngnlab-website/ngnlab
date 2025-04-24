a3fcfb9c3cf954a823f6994b76d0d70dc6be075f
Mugundh <mugundhjb@gmail.com>
2025-04-24 14:06:16 +0530

    Merge master & Added UAV research highlights

---
+++ scripts/run.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+fs.readFile('data/newsRoom.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading JSON file:', err);
+        return;
+    }
+    try {
+        const jsonData = JSON.parse(data);
+        console.log('JSON Data:', jsonData);
+    } catch (parseError) {
+        console.error('Error parsing JSON:', parseError);
+    }
+});
