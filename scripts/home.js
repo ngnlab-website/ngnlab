0f682d85a1c4c72187f7efee60617cd44266770f
Shubha845 <shubhakarini.in@gmail.com>
2025-06-02 23:17:35 +0530

    Update home.js 2.6.25

--- scripts/home.js
+++ scripts/home.js
@@ -18,53 +18,57 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     }
-let slideIndex = 0;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+    let slideIndex = 0;
+    showSlides(slideIndex);
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }    
-    if (n < 1) {
-        slideIndex = slides.length
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
     }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" actives", "");
-    }
-    slides[slideIndex-1].style.display = "flex";  
-    dots[slideIndex-1].className += " actives";
-    // setTimeout(showSlides(slideIndex-1), 2000);
-}
-let autoSlideIndex = 0;
-autoShowSlides();
 
-function autoShowSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" actives", "");
-  }
-  slides[slideIndex-1].style.display = "flex";  
-  dots[slideIndex-1].className += " actives";
-  setTimeout(autoShowSlides, 5000); // Change image every 2 seconds
-}
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {
+            slideIndex = 1;
+        }    
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" actives", "");
+        }
+        slides[slideIndex - 1].style.display = "flex";  
+        dots[slideIndex - 1].className += " actives";
+    }
+
+    let autoSlideIndex = 0;
+    autoShowSlides();
+
+    function autoShowSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        autoSlideIndex++;
+        if (autoSlideIndex > slides.length) {
+            autoSlideIndex = 1;
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" actives", "");
+        }
+        slides[autoSlideIndex - 1].style.display = "flex";  
+        dots[autoSlideIndex - 1].className += " actives";
+        setTimeout(autoShowSlides, 5000); // Change slide every 5 seconds
+    }
+});
