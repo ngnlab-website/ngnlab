1d2eb49b0bf67293c3e1a04cc552991abfe08e33
Shubha845 <shubhakarini.in@gmail.com>
2026-01-31 13:13:52 +0530

    31-01-26

--- scripts/consultancyProjects.js
+++ scripts/consultancyProjects.js
@@ -1,53 +1,30 @@
-const fundedProjectsSectionContainer = document.querySelector(".funded-projects-section-container")
-const patentsSectionContainer = document.querySelector(".funded-projects-section-container2")
-// import fundedProjects from "../data/fundedProjects.json" assert { type: "json" }
-let fundedProjects = []
+const consultancyProjectsSectionContainer = document.querySelector(
+  ".consultancy-projects-section-container"
+);
+
+let consultancyProjects = [];
+
 fetch("../data/consultancyProjects.json")
-    .then(response => response.json())
-    .then(data => {
-        fundedProjects=data
-        renderProjects()
-    })
-    .catch(error => {
-        console.error("Error fetching JSON:", error);
-    });
-function renderProjects() {
-    fundedProjects.forEach(fundedProject => {
-        let proj =  `
-                        <div class="funded-project"> 
-                            <div class="description">${fundedProject.title}</div>
-                            <div class="other-details">
-                                <span class="funding-agency">${fundedProject.agency}</span>
-                            </div>
-                        </div>
-                    `
-        fundedProjectsSectionContainer.innerHTML += proj
-    })
-}
-let patents = []
-fetch("../data/patents.json")
-    .then(response => response.json())
-    .then(data => {
-        patents=data
-        renderPatents()
-    })
-    .catch(error => {
-        console.error("Error fetching JSON:", error);
-    });
-function renderPatents() {
-    patents.forEach(patent => {
-        let pat =   `
-                        <div class="funded-project"> 
-                            <div class="description">${patent.title}</div>
-                            <div class="other-details">
-                                <!--<span class="funding-agency">Status: ${patent.status}</span> <br>-->
-                                <span class="funding-agency" style="color: maroon;">Inventors:</span>
-                                <span class="funding-agency">${patent.inventors}</span><br>
-                                <span class="funding-agency" style="color: maroon;">${patent.status}: </span>
-                                <span class="funding-agency">${patent.year}</span><br>
-                            </div>
-                        </div>
-                    `
-        patentsSectionContainer.innerHTML += pat
-    })
+  .then((response) => response.json())
+  .then((data) => {
+    consultancyProjects = data;
+    renderConsultancyProjects();
+  })
+  .catch((error) => {
+    console.error("Error fetching consultancy projects JSON:", error);
+  });
+
+function renderConsultancyProjects() {
+  consultancyProjects.forEach((project) => {
+    const proj = `
+      <div class="consultancy-project">
+        <div class="description">${project.title}</div>
+        <div class="other-details">
+          <span class="funding-agency">${project.agency}</span>
+        </div>
+        <div class="year">${project.year}</div>
+      </div>
+    `;
+    consultancyProjectsSectionContainer.innerHTML += proj;
+  });
 }
